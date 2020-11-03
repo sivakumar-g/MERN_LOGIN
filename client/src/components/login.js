164d530ad6d361c63c263ccf00e57c9c0dd33764
sivakumar-g <sivakuhan98@gmail.com>
2020-11-03 19:04:12 +0530

    first

---
+++ client/src/components/login.js
@@ -0,0 +1,138 @@
+import React, { Component,useState } from "react";
+import axios from 'axios'
+import { useDispatch } from 'react-redux';
+import { ACTION_TYPES, loginUser,registerUser,loginuseraction } from '../actions/users';
+import user from "../reducers/user";
+import { connect ,useSelector} from "react-redux";
+
+
+
+
+ function Login(props) {
+
+    const dispatch = useDispatch();
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+const onEmailHandler = (e)=>{
+    setEmail(e.currentTarget.value);
+}
+const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value)
+}
+const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    let dataToSubmit = {
+        email: Email,
+        password: Password
+    }
+ 
+       axios.post('http://localhost:4000/users/authenticate', dataToSubmit)
+       .then(res=>
+           {// console.log(res.data.success);
+               dispatch(loginuseraction(res.data));
+               if (res.data) {
+                alert('Logged In Successfull');
+                // console.log(res.data.token);
+                localStorage.setItem('token',res.data.token);
+                window.location.reload(true);
+                props.history.push('/ticket')
+            } else {
+                alert('Error˝')
+            }
+
+           })
+
+
+
+}
+        return (
+            <form  onSubmit={onSubmitHandler}>
+                <h3>Sign In</h3>
+
+
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" placeholder="Enter email" 
+                    value={Email} onChange={onEmailHandler}
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" 
+                    value={Password} onChange={onPasswordHandler}
+                    />
+                </div>
+
+              
+                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+               
+            </form>
+        );
+    }
+
+    
+const mapStateToProps = state => ({
+    login : state.user.loginSuccess
+})
+
+const mapActionToProps = {
+loginaction : loginuseraction
+}
+
+    export default connect(mapStateToProps, mapActionToProps)(Login)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   //props.loginaction(dataToSubmit)
+  /*  var logindata = null;
+    logindata= dispatch(loginUser(dataToSubmit)).then(response=>
+       { console.log(response)});
+*/
---
+++ client/src/components/signup.js
@@ -0,0 +1,100 @@
+import React, { Component ,useState} from "react";
+import axios from 'axios'
+import { useDispatch } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { loginUser,registerUser, registeruseraction } from '../actions/users';
+
+
+function Signup(props) {
+
+    const dispatch = useDispatch();
+
+    const [Email, setEmail] = useState("")
+    const [Name, setName] = useState("")
+    const [Password, setPassword] = useState("")
+    
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+
+    const onNameHandler = (event) => {
+        setName(event.currentTarget.value)
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        let dataToSubmit = {
+            email: Email,
+            password: Password,
+            name: Name,
+        }
+       /*var registerdata = null; 
+       registerdata = dispatch(registerUser(dataToSubmit))
+       */
+      axios.post('http://localhost:4000/users/register', dataToSubmit)
+      .then(res=>
+          {console.log(res.data.success);
+              dispatch(registeruseraction(res.data));
+              if (res.data) {
+               alert('Sign UP Successfull');
+               props.history.push('/sign-in')
+           } else {
+               alert('Error˝')
+           }
+
+          })
+      
+        
+    }
+
+
+    return (
+        <form onSubmit={onSubmitHandler}
+        >
+            <h3>Sign Up</h3>
+
+            <div className="form-group">
+                <label> name</label>
+                <input type="text" className="form-control" placeholder="name" 
+                 value={Name} onChange={onNameHandler}
+                />
+            </div>
+
+           
+            <div className="form-group">
+                <label>Email address</label>
+                <input type="email" className="form-control" placeholder="Enter email" 
+                value={Email}  onChange={onEmailHandler}
+                />
+            </div>
+
+            <div className="form-group">
+                <label>Password</label>
+                <input type="password" className="form-control" placeholder="Enter password" 
+                value={Password} onChange={onPasswordHandler}/>
+            </div>
+
+            <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+          
+        </form>
+    );
+}
+export default withRouter(Signup)
+
+
+
+
+
+
+
+
+
+
+
+
